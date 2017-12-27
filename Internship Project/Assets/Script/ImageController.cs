b4a1c3576e5ca71cce12674b29d64a99fd5e9fb0
Henoch Yanuary <henochyanuary@gmail.com>
2017-12-27 15:46:34 +1000

    adding in some images to help the visual aesthetci

---
+++ Internship Project/Assets/Script/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageController : MonoBehaviour {
+
+    public Image mainImage;
+    public List<Sprite> sprites = new List<Sprite>();
+
+    private DataController dataController;
+	// Use this for initialization
+	void Start () {
+        dataController = FindObjectOfType<DataController>();
+        mainImage.sprite = sprites[dataController.numberLevel];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
